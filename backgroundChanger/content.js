c7e36377e5c20c774fdb7fbc31c033a314a9c871
vineelgajjala <gajjala.vineel@gmail.com>
2022-02-06 22:25:51 -0600

    srikar image added

---
+++ backgroundChanger/content.js
@@ -0,0 +1,11 @@
+document.body.style.backgroundImage = "url('srikar.PNG')";
+
+//alert("Hey")
+
+// so, i want to update the background to play a video here, that feels like html updating over anything else. This is called on the page being loaded, so now i need to 
+
+//useful websites:
+
+// https://www.w3schools.com/js/js_htmldom_html.asp
+
+// https://www.w3schools.com/howto/howto_css_fullscreen_video.asp
